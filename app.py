d5353cf69428cc93d63583d7b153ab4dcbf3af1d
funnygamerz444-blip <funnygamerz444@gmail.com>
2026-07-31 01:33:08 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+  return 'Render Backend Server is Live and Running for Yeaxin Panel!'
+
+
 @app.route('/api/player')
 def proxy_player():
   uid = request.args.get('uid')
@@ -19,3 +24,7 @@ def proxy_player():
     pass
 
   return jsonify({'success': False, 'error': 'Failed to fetch'})
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
